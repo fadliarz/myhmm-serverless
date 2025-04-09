7ddce9bb1d0e50a7f4be29d1d0d5568898ed7833
fadliarz <fadliarz03@gmail.com>
2025-04-09 21:58:00 +0700

    feat: add type

---
+++ src/common/CompletionStatus.ts
@@ -0,0 +1,5 @@
+export enum CompletionStatus {
+  NOT_STARTED = 'NOT_STARTED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
---
+++ src/common/EventName.ts
@@ -0,0 +1,4 @@
+export enum EventName {
+  'INSERT' = 'INSERT',
+  'REMOVE' = 'REMOVE'
+}
