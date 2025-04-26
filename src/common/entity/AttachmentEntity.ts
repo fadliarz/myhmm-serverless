2336cfcea5cc7bab5e694162cbedd7bb73c527ac
fadliarz <fadliarz03@gmail.com>
2025-04-26 23:29:11 +0700

    chore: add entities

---
+++ src/common/entity/AttachmentEntity.ts
@@ -0,0 +1,10 @@
+export default class AttachmentEntity {
+  public attachmentId!: number;
+  public lessonId!: number;
+  public courseId!: number;
+  public name!: string;
+  public description!: string;
+  public file!: string;
+  public createdAt!: string;
+  public updatedAt!: string;
+}
---
+++ src/common/entity/VideoEntity.ts
@@ -0,0 +1,11 @@
+export default class VideoEntity {
+  public videoId!: number;
+  public courseId!: number;
+  public lessonId!: number;
+  public title!: string;
+  public description!: string;
+  public durationInSec!: number;
+  public youtubeLink!: string;
+  public createdAt!: string;
+  public updatedAt!: string;
+}
