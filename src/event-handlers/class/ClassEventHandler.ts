80ee18a3e5d18bb54fc1a62027b6fab71b1fb85a
fadliarz <fadliarz03@gmail.com>
2025-05-08 22:11:48 +0700

    feat(ClassDeletedEventHandler): implement delete class assignments and enrollments

--- src/event-handlers/class/ClassEventHandler.ts
+++ src/event-handlers/class/ClassEventHandler.ts
@@ -3,9 +3,19 @@ import 'dotenv/config';
 
 
 export default abstract class ClassEventHandler {
-  public async getEnv(): Promise<{ CLASS_ASSIGNMENT_TABLE: string }> {
+  public async getEnv(): Promise<{
+    CLASS_ASSIGNMENT_TABLE: string,
+    ENROLLMENT_TABLE: string,
+    ENROLLMENT_TABLE_GSI: string,
+    INSTRUCTOR_TABLE: string
+    INSTRUCTOR_TABLE_GSI: string
+  }> {
     return cleanEnv(process.env, {
       CLASS_ASSIGNMENT_TABLE: str(),
+      ENROLLMENT_TABLE: str(),
+      ENROLLMENT_TABLE_GSI: str(),
+      INSTRUCTOR_TABLE: str(),
+      INSTRUCTOR_TABLE_GSI: str(),
     });
   }
 };
--- src/event-handlers/class/on-delete/ClassDeletedEventHandler.ts
+++ src/event-handlers/class/on-delete/ClassDeletedEventHandler.ts
@@ -6,6 +6,8 @@ import ClassEventHandler from '../ClassEventHandler';
 import ClassDeletedEvent from '../event/ClassDeletedEvent';
 import ClassEntity from '../../../common/entity/ClassEntity';
 import ClassAssignmentEntity from '../../../common/entity/ClassAssignmentEntity';
+import EnrollmentEntity from '../../../common/entity/EnrollmentEntity';
+import InstructorEntity from '../../../common/entity/InstructorEntity';
 
 export default class ClassDeletedEventHandler extends ClassEventHandler {
   private readonly dynamoDBDocumentClient: DynamoDBDocumentClient = generateDynamoDBDocumentClient();
@@ -13,6 +15,7 @@ export default class ClassDeletedEventHandler extends ClassEventHandler {
   public async handle(classDeletedEvent: ClassDeletedEvent) {
     const deletedClassEntity: ClassEntity = classDeletedEvent.data.OldImage;
     await this.deleteClassAssignments({ classId: deletedClassEntity.classId, courseId: deletedClassEntity.courseId });
+    await this.deleteEnrollments({ classId: deletedClassEntity.classId, courseId: deletedClassEntity.courseId });
   }
 
   private async deleteClassAssignments(param: { classId: number, courseId: number }): Promise<void> {
@@ -70,6 +73,136 @@ export default class ClassDeletedEventHandler extends ClassEventHandler {
       } catch (exception) {
         console.info('[ClassDeletedEventHandler:removeCourseCategory] Exception thrown:', exception);
         console.info(`[ClassDeletedEventHandler:removeCourseCategory] Attempting to retry (${RETRIES})...`);
+        RETRIES++;
+        if (RETRIES > MAX_RETRIES) {
+          throw new MaxRetriesException(exception as Error);
+        }
+        await TimerService.sleepWith100MsBaseDelayExponentialBackoff(RETRIES);
+      }
+    }
+  }
+
+  private async deleteEnrollments(param: { classId: number, courseId: number }): Promise<void> {
+    const { classId, courseId } = param;
+    const env = await this.getEnv();
+    let lastEvaluatedKey: Record<string, any> | undefined = undefined;
+    do {
+      const {
+        Items: enrollmentEntities,
+        LastEvaluatedKey,
+      } = await this.dynamoDBDocumentClient.send(new QueryCommand({
+        TableName: env.ENROLLMENT_TABLE,
+        IndexName: env.ENROLLMENT_TABLE_GSI,
+        KeyConditionExpression: '#classId = :value0',
+        ExpressionAttributeNames: {
+          '#classId': 'classId',
+        },
+        ExpressionAttributeValues: {
+          ':value0': classId,
+        },
+        ExclusiveStartKey: lastEvaluatedKey,
+      }));
+      console.info(`[ClassDeletedEventHandler] Retrieved ${enrollmentEntities?.length ?? 0} enrollments`);
+      if (enrollmentEntities) {
+        for (const enrollmentEntity of enrollmentEntities as EnrollmentEntity[]) {
+          if (enrollmentEntity.courseId === courseId) {
+            await this.deleteEnrollment({
+              key: {
+                userId: enrollmentEntity.userId,
+                classId,
+              },
+            });
+          }
+        }
+      }
+      lastEvaluatedKey = LastEvaluatedKey as any;
+    } while (lastEvaluatedKey);
+  }
+
+  private async deleteEnrollment(param: {
+    key: {
+      userId: number,
+      classId: number
+    }
+  }): Promise<void> {
+    const env = await this.getEnv();
+    let RETRIES: number = 0;
+    const MAX_RETRIES: number = 3;
+    while (RETRIES <= MAX_RETRIES) {
+      try {
+        await this.dynamoDBDocumentClient.send(new DeleteCommand({
+          TableName: env.ENROLLMENT_TABLE,
+          Key: param.key,
+        }));
+        return;
+      } catch (exception) {
+        console.info('[ClassDeletedEventHandler:removeCourseCategory] Exception thrown:', exception);
+        console.info(`[ClassDeletedEventHandler:removeCourseCategory] Attempting to retry (${RETRIES})...`);
+        RETRIES++;
+        if (RETRIES > MAX_RETRIES) {
+          throw new MaxRetriesException(exception as Error);
+        }
+        await TimerService.sleepWith100MsBaseDelayExponentialBackoff(RETRIES);
+      }
+    }
+  }
+
+  private async deleteInstructors(param: { classId: number, courseId: number }): Promise<void> {
+    const { classId, courseId } = param;
+    const env = await this.getEnv();
+    let lastEvaluatedKey: Record<string, any> | undefined = undefined;
+    do {
+      const {
+        Items: instructorEntities,
+        LastEvaluatedKey,
+      } = await this.dynamoDBDocumentClient.send(new QueryCommand({
+        TableName: env.INSTRUCTOR_TABLE,
+        IndexName: env.INSTRUCTOR_TABLE_GSI,
+        KeyConditionExpression: '#classId = :value0',
+        ExpressionAttributeNames: {
+          '#classId': 'classId',
+        },
+        ExpressionAttributeValues: {
+          ':value0': classId,
+        },
+        ExclusiveStartKey: lastEvaluatedKey,
+      }));
+      console.info(`[ClassDeletedEventHandler] Retrieved ${instructorEntities?.length ?? 0} instructors`);
+      if (instructorEntities) {
+        for (const instructorEntity of instructorEntities as InstructorEntity[]) {
+          if (instructorEntity.courseId === courseId) {
+            await this.deleteInstructor({
+              key: {
+                userId: instructorEntity.userId,
+                classId,
+              },
+            });
+          }
+        }
+      }
+      lastEvaluatedKey = LastEvaluatedKey as any;
+    } while (lastEvaluatedKey);
+  }
+
+  private async deleteInstructor(param: {
+    key: {
+      userId: number,
+      classId: number
+    }
+  }): Promise<void> {
+    const env = await this.getEnv();
+    let RETRIES: number = 0;
+    const MAX_RETRIES: number = 3;
+    while (RETRIES <= MAX_RETRIES) {
+      try {
+        await this.dynamoDBDocumentClient.send(new DeleteCommand({
+          TableName: env.INSTRUCTOR_TABLE,
+          Key: param.key,
+        }));
+        return;
+      } catch (exception) {
+        console.info('[ClassDeletedEventHandler:deleteInstructor] Exception thrown:', exception);
+        console.info(`[ClassDeletedEventHandler:deleteInstructor] Attempting to retry (${RETRIES})...`);
         RETRIES++;
         if (RETRIES > MAX_RETRIES) {
           throw new MaxRetriesException(exception as Error);
