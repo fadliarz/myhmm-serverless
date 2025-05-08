967f99be214bb380a396da93d158769341e52ae8
fadliarz <fadliarz03@gmail.com>
2025-05-08 18:08:23 +0700

    fix: fix delay time calculation

--- src/common/TimerService.ts
+++ src/common/TimerService.ts
@@ -7,7 +7,7 @@ export default class TimerService {
         () => {
           resolve();
         },
-        1000 + Math.pow(2, attempt) + Math.floor(Math.random() * 1000),
+        1000 * Math.pow(2, attempt) + Math.floor(Math.random() * 1000),
       );
     });
   }
