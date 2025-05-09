a8c99c8890851ccad4eb453e5e2b7ee57a9190ce
fadliarz <fadliarz03@gmail.com>
2025-05-09 15:40:12 +0700

    feat: update counter if course exists

--- src/event-handlers/lesson/LessonEventHandler.ts
+++ src/event-handlers/lesson/LessonEventHandler.ts
@@ -3,10 +3,11 @@ import 'dotenv/config';
 
 
 export default abstract class LessonEventHandler {
-  public async getEnv(): Promise<{ VIDEO_TABLE: string, ATTACHMENT_TABLE: string }> {
+  public async getEnv(): Promise<{ VIDEO_TABLE: string, ATTACHMENT_TABLE: string, COURSE_TABLE: string }> {
     return cleanEnv(process.env, {
       VIDEO_TABLE: str(),
       ATTACHMENT_TABLE: str(),
+      COURSE_TABLE: str(),
     });
   }
 };
--- src/event-handlers/lesson/on-delete/LessonDeletedEventHandler.ts
+++ src/event-handlers/lesson/on-delete/LessonDeletedEventHandler.ts
@@ -1,4 +1,10 @@
-import { DeleteCommand, DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  QueryCommand,
+  TransactWriteCommand,
+} from '@aws-sdk/lib-dynamodb';
 import generateDynamoDBDocumentClient from '../../../common/generateDynamoDBDocumentClient';
 import TimerService from '../../../common/TimerService';
 import MaxRetriesException from '../../../common/MaxRetriesException';
@@ -7,6 +13,9 @@ import LessonDeletedEvent from '../event/LessonDeletedEvent';
 import LessonEntity from '../../../common/entity/LessonEntity';
 import VideoEntity from '../../../common/entity/VideoEntity';
 import AttachmentEntity from '../../../common/entity/AttachmentEntity';
+import CourseKey from '../../../common/entity/CourseKey';
+import VideoKey from '../../../common/entity/VideoKey';
+import AttachmentKey from '../../../common/entity/AttachmentKey';
 
 export default class LessonDeletedEventHandler extends LessonEventHandler {
   private readonly dynamoDBDocumentClient: DynamoDBDocumentClient = generateDynamoDBDocumentClient();
@@ -20,7 +29,6 @@ export default class LessonDeletedEventHandler extends LessonEventHandler {
   private async deleteVideos(param: { lessonId: number }): Promise<void> {
     const { lessonId } = param;
     const env = await this.getEnv();
-    let deletedVideoCount: number = 0;
     let lastEvaluatedKey: Record<string, any> | undefined = undefined;
     do {
       const {
@@ -37,39 +45,88 @@ export default class LessonDeletedEventHandler extends LessonEventHandler {
         },
         ExclusiveStartKey: lastEvaluatedKey,
       }));
+      console.info(`[LessonDeletedEventHandler] Retrieved ${videoEntities?.length ?? 0} videos`);
       if (videoEntities) {
         for (const videoEntity of videoEntities as VideoEntity[]) {
           await this.deleteVideo({
             key: {
+              courseId: videoEntity.courseId,
               lessonId,
               videoId: videoEntity.videoId,
+              durationInSec: videoEntity.durationInSec,
             },
           });
-          deletedVideoCount++;
         }
       }
       lastEvaluatedKey = LastEvaluatedKey as any;
     } while (lastEvaluatedKey);
-    console.info('@LessonDeletedEventHandler * successfully deleted ' + deletedVideoCount + ' videos!');
   }
 
   private async deleteVideo(param: {
     key: {
+      courseId: number,
       lessonId: number,
-      videoId: number
+      videoId: number,
+      durationInSec: number,
     }
   }): Promise<void> {
+    const { courseId, lessonId, videoId, durationInSec } = param.key;
     const env = await this.getEnv();
     let RETRIES: number = 0;
     const MAX_RETRIES: number = 3;
     while (RETRIES <= MAX_RETRIES) {
       try {
-        await this.dynamoDBDocumentClient.send(new DeleteCommand({
-          TableName: env.VIDEO_TABLE,
-          Key: param.key,
-        }));
+        const { Item: courseEntity } = await this.dynamoDBDocumentClient.send(
+          new GetCommand({
+            TableName: env.COURSE_TABLE,
+            Key: new CourseKey({ courseId }),
+          }),
+        );
+        if (!courseEntity) {
+          await this.dynamoDBDocumentClient.send(new DeleteCommand({
+            TableName: env.VIDEO_TABLE,
+            Key: new VideoKey({ lessonId, videoId }),
+          }));
+          return;
+        }
+        await this.dynamoDBDocumentClient.send(
+          new TransactWriteCommand({
+            TransactItems: [
+              {
+                Delete: {
+                  TableName: env.VIDEO_TABLE,
+                  Key: new VideoKey({ lessonId, videoId }),
+                },
+              },
+              {
+                Update: {
+                  TableName: env.COURSE_TABLE,
+                  Key: new CourseKey({ courseId }),
+                  ConditionExpression:
+                    'attribute_exists(id) AND attribute_exists(courseId) AND #numberOfDurations = :value0 AND #numberOfVideos = :value1',
+                  UpdateExpression:
+                    'SET #numberOfDurations = :value2, #numberOfVideos = :value3',
+                  ExpressionAttributeNames: {
+                    '#numberOfVideos': 'numberOfVideos',
+                    '#numberOfDurations': 'numberOfDurations',
+                  },
+                  ExpressionAttributeValues: {
+                    ':value0': courseEntity.numberOfDurations,
+                    ':value1': courseEntity.numberOfVideos,
+                    ':value2':
+                      courseEntity.numberOfDurations -
+                      durationInSec,
+                    ':value3': courseEntity.numberOfVideos - 1,
+                  },
+                },
+              },
+            ],
+          }),
+        );
         return;
       } catch (exception) {
+        console.info('[LessonDeletedEventHandler:deleteVideo] Exception thrown:', exception);
+        console.info(`[LessonDeletedEventHandler:deleteVideo] Attempting to retry (${RETRIES})...`);
         RETRIES++;
         if (RETRIES > MAX_RETRIES) {
           throw new MaxRetriesException(exception as Error);
@@ -82,7 +139,6 @@ export default class LessonDeletedEventHandler extends LessonEventHandler {
   private async deleteAttachments(param: { lessonId: number }): Promise<void> {
     const { lessonId } = param;
     const env = await this.getEnv();
-    let deletedAttachmentCount: number = 0;
     let lastEvaluatedKey: Record<string, any> | undefined = undefined;
     do {
       const {
@@ -99,39 +155,79 @@ export default class LessonDeletedEventHandler extends LessonEventHandler {
         },
         ExclusiveStartKey: lastEvaluatedKey,
       }));
+      console.info(`[LessonDeletedEventHandler] Retrieved ${attachmentEntities?.length ?? 0} attachments`);
       if (attachmentEntities) {
         for (const attachmentEntity of attachmentEntities as AttachmentEntity[]) {
           await this.deleteAttachment({
             key: {
+              courseId: attachmentEntity.courseId,
               lessonId,
               attachmentId: attachmentEntity.attachmentId,
             },
           });
-          deletedAttachmentCount++;
         }
       }
       lastEvaluatedKey = LastEvaluatedKey as any;
     } while (lastEvaluatedKey);
-    console.info('@LessonDeletedEventHandler * successfully deleted ' + deletedAttachmentCount + ' attachments!');
   }
 
   private async deleteAttachment(param: {
     key: {
+      courseId: number,
       lessonId: number,
       attachmentId: number
     }
   }): Promise<void> {
+    const { courseId, lessonId, attachmentId } = param.key;
     const env = await this.getEnv();
     let RETRIES: number = 0;
     const MAX_RETRIES: number = 3;
     while (RETRIES <= MAX_RETRIES) {
       try {
-        await this.dynamoDBDocumentClient.send(new DeleteCommand({
-          TableName: env.ATTACHMENT_TABLE,
-          Key: param.key,
-        }));
+        const { Item: courseEntity } = await this.dynamoDBDocumentClient.send(
+          new GetCommand({
+            TableName: env.COURSE_TABLE,
+            Key: new CourseKey({ courseId }),
+          }),
+        );
+        if (!courseEntity) {
+          await this.dynamoDBDocumentClient.send(new DeleteCommand({
+            TableName: env.ATTACHMENT_TABLE,
+            Key: new AttachmentKey({ lessonId, attachmentId }),
+          }));
+          return;
+        }
+        await this.dynamoDBDocumentClient.send(
+          new TransactWriteCommand({
+            TransactItems: [
+              {
+                Delete: {
+                  TableName: env.ATTACHMENT_TABLE,
+                  Key: new AttachmentKey({ lessonId, attachmentId }),
+                },
+              },
+              {
+                Update: {
+                  TableName: env.COURSE_TABLE,
+                  Key: new CourseKey({ courseId }),
+                  ConditionExpression:
+                    'attribute_exists(id) AND attribute_exists(courseId)',
+                  UpdateExpression: 'ADD #numberOfAttachments :value0',
+                  ExpressionAttributeNames: {
+                    '#numberOfAttachments': 'numberOfAttachments',
+                  },
+                  ExpressionAttributeValues: {
+                    ':value0': -1,
+                  },
+                },
+              },
+            ],
+          }),
+        );
         return;
       } catch (exception) {
+        console.info('[LessonDeletedEventHandler:deleteAttachment] Exception thrown:', exception);
+        console.info(`[LessonDeletedEventHandler:deleteAttachment] Attempting to retry (${RETRIES})...`);
         RETRIES++;
         if (RETRIES > MAX_RETRIES) {
           throw new MaxRetriesException(exception as Error);
