d7ce935bdaa33f8384596c6faa1c675cc10a6d1f
fadliarz <fadliarz03@gmail.com>
2025-05-21 11:38:49 +0700

    feat: handle CourseScheduleEvent

--- src/event-handlers/enrollment-and-class-assignment/handleEnrollmentAndClassAssignmentEvent.ts
+++ src/event-handlers/enrollment-and-class-assignment/handleEnrollmentAndClassAssignmentEvent.ts
@@ -10,6 +10,10 @@ import ClassAssignmentCreatedEventHandler from '../class-assignment/on-create/Cl
 import ClassAssignmentCreatedEvent from '../class-assignment/event/ClassAssignmentCreatedEvent';
 import ClassAssignmentDeletedEventHandler from '../class-assignment/on-delete/ClassAssignmentDeletedEventHandler';
 import ClassAssignmentDeletedEvent from '../class-assignment/event/ClassAssignmentDeletedEvent';
+import CourseScheduleCreatedEvent from '../course-schedule/event/CourseScheduleCreatedEvent';
+import CourseScheduleCreatedEventHandler from '../course-schedule/on-create/CourseScheduleCreatedEventHandler';
+import CourseScheduleDeletedEventHandler from '../course-schedule/on-delete/CourseScheduleDeletedEventHandler';
+import CourseScheduleDeletedEvent from '../course-schedule/event/CourseScheduleDeletedEvent';
 
 export const handleEnrollmentAndClassAssignmentEvent: SQSHandler = async (
   event: SQSEvent,
@@ -18,7 +22,7 @@ export const handleEnrollmentAndClassAssignmentEvent: SQSHandler = async (
     const body = JSON.parse(record.body);
     const { eventName, dynamodb } = body;
     const { Keys, NewImage, OldImage } = dynamodb;
-    if ((Keys as object).hasOwnProperty('userId')) {
+    if ((Keys as object).hasOwnProperty('userId') && (Keys as object).hasOwnProperty('classId')) {
       if (eventName === EventName.INSERT) {
         const enrollmentCreatedEventHandler: EnrollmentCreatedEventHandler = new EnrollmentCreatedEventHandler();
         const enrollmentCreatedEvent: EnrollmentCreatedEvent = new EnrollmentCreatedEvent({ NewImage: unmarshall(NewImage) as any });
@@ -31,6 +35,25 @@ export const handleEnrollmentAndClassAssignmentEvent: SQSHandler = async (
         console.info('[Handler] Handling EnrollmentDeletedEvent:', JSON.stringify(enrollmentDeletedEvent));
         await enrollmentDeletedEventHandler.handle(enrollmentDeletedEvent);
         console.info('[Handler] Handled EnrollmentDeletedEvent:', JSON.stringify(enrollmentDeletedEvent));
+      } else {
+        throw new Error(`[Handler] Event name "${eventName}" is not supported`);
+      }
+      return;
+    }
+
+    if ((Keys as object).hasOwnProperty('courseId') && (Keys as object).hasOwnProperty('scheduleId')) {
+      if (eventName === EventName.INSERT) {
+        const courseScheduleCreatedEventHandler: CourseScheduleCreatedEventHandler = new CourseScheduleCreatedEventHandler();
+        const courseScheduleCreatedEvent: CourseScheduleCreatedEvent = new CourseScheduleCreatedEvent({ NewImage: unmarshall(NewImage) as any });
+        console.info('[Handler] Handling CourseScheduleCreatedEvent:', JSON.stringify(courseScheduleCreatedEvent));
+        await courseScheduleCreatedEventHandler.handle(courseScheduleCreatedEvent);
+        console.info('[Handler] Handled CourseScheduleCreatedEvent:', JSON.stringify(courseScheduleCreatedEvent));
+      } else if (eventName === EventName.REMOVE) {
+        const courseScheduleDeletedEventHandler: CourseScheduleDeletedEventHandler = new CourseScheduleDeletedEventHandler();
+        const courseScheduleDeletedEvent: CourseScheduleDeletedEvent = new CourseScheduleDeletedEvent({ OldImage: unmarshall(OldImage) as any });
+        console.info('[Handler] Handling CourseScheduleDeletedEvent:', JSON.stringify(courseScheduleDeletedEvent));
+        await courseScheduleDeletedEventHandler.handle(courseScheduleDeletedEvent);
+        console.info('[Handler] Handled CourseScheduleDeletedEvent:', JSON.stringify(courseScheduleDeletedEvent));
       } else {
         throw new Error(`[Handler] Event name "${eventName}" is not supported`);
       }
